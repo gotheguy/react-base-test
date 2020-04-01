73b7401edd7eaf7dbfd1a6a3b16a4be544ac4dc9
gotheguy <gonzalo.s.otheguy@gmail.com>
2020-04-01 20:28:56 -0300

    CSS Modules

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,32 +1,13 @@
 import React from 'react';
-//import './Person.css';
-import styled from 'styled-components';
-
-const StyledDiv = styled.div`
-                    width: 30%;
-                    margin: auto;
-                    border: 1px solid #eee;
-                    box-shadow: 0 2px 3px #ccc;
-                    padding: 16px;
-                    text-align: center;
-                    cursor: pointer;
-
-                    @media (min-width: 500px) {
-                    width: 450px;
-                    }`;
+import classes from'./Person.css';
 
 const person = (props) => {
-    const style = {
-        '@media(minWidth: 500px)': {
-            width: '450px'
-        }
-    };
     return (
-       <StyledDiv>
+       <div className={classes.Person}>
             <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name} />
-        </StyledDiv>
+        </div>
     )
 }
 
